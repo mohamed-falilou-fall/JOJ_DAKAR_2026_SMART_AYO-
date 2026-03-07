2dd63cf7b2e94abc4c962d92dceeb6df23d1d453
Mohamed Falilou Fall <mff.falilou.fall@gmail.com>
2026-03-07 16:31:10 +0000

    Delete modules/optimisation_volontaires.py

--- modules/optimisation_volontaires.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-def taux_couverture(df):
-
-    total_requis=df["Volontaires_requis"].sum()
-    total_actuels=df["Volontaires_actuels"].sum()
-
-    return round((total_actuels/total_requis)*100,2)
